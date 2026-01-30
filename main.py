db45470510e52ec59723ea8baf477ad3bf765953
kuzakbaev6-tech <kuzakbaev6@gmail.com>
2026-01-30 15:21:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("8501528225:AAHFlN2aS6DMR6cx-uscnUciYgApUpxhQQI")
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот тірі 👍")
